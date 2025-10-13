be2ca91b37dcc6abfc6c52a79e877a2fd26cac98
ales27pm <56891139+ales27pm@users.noreply.github.com>
2025-10-13 15:09:28 -0400

    Fix port availability check to avoid false positives

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -105,7 +105,6 @@ import sys
 port = int(sys.argv[1])
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     try:
         sock.bind(("0.0.0.0", port))
     except OSError:
