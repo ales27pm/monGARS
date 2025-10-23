81a919a0156b744bfb5236955203d60a7c9f1b1b
ales27pm <56891139+ales27pm@users.noreply.github.com>
2025-10-23 19:27:48 -0400

    Relax mypy import traversal and fix optional backends

--- typings/prefect/__init__.pyi
+++ typings/prefect/__init__.pyi
@@ -4,14 +4,17 @@ from typing import Any, Callable, TypeVar, overload
 
 _F = TypeVar("_F", bound=Callable[..., Any])
 
+
 @overload
-def flow(__fn: _F) -> _F: ...
+def flow(__fn: _F, /) -> _F: ...
+
+
 @overload
 def flow(
     *,
     name: str | None = ...,
     **kwargs: Any,
 ) -> Callable[[_F], _F]: ...
-def flow(*args: Any, **kwargs: Any) -> Any: ...
+
 
 __all__ = ["flow"]
