134a330535f1e124fdfa92c9c57f0cad53f16323
ales27pm <56891139+ales27pm@users.noreply.github.com>
2025-11-23 19:32:36 -0500

    Ensure test dependencies installed

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
         "requests-oauthlib>=2.0.0",
         "websocket-client>=1.8.0",
         "websockets>=12.0",
+        "pytest-asyncio>=0.23.8,<0.25",
     ],
     extras_require={
         "grpo": [
